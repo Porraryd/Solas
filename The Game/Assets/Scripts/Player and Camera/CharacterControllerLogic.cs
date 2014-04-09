c056ce4ab3bb00ccfb1cefd3006edd1c8cf4d23b
Pontus <ponor358@student.liu.se>
2014-04-09 14:59:03 +0200

    Grundversion av kamera och kontroll

---
+++ The Game/Assets/Scripts/Player and Camera/CharacterControllerLogic.cs
@@ -0,0 +1,106 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterControllerLogic : MonoBehaviour {
+
+	private Animator animator;
+
+	public float directionDampTime = .25f;
+	public ThirdPersonCamera gameCamera;
+	public float directionSpeed = 3.0f;
+	public float rotationDegreePerSecond = 120f;
+	public float rotationSpeed = 10f;
+
+	private float speed = 0.0f;
+	private float horizontal = 0.0f;
+	private float vertical = 0.0f;
+	private AnimatorStateInfo stateInfo;
+
+	private int m_LocomotionId = 0;
+
+
+	void Start () {
+
+		animator = GetComponent<Animator>();
+
+		if(animator.layerCount >= 2)
+		{
+			animator.SetLayerWeight(1, 1);
+		}	
+
+		//hash the locomotion id for performance 
+		m_LocomotionId = Animator.StringToHash ("Base Layer.Locomotion");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		stateInfo = animator.GetCurrentAnimatorStateInfo(0);
+
+		//get input from controller
+		horizontal = Input.GetAxis("Horizontal");
+		vertical = Input.GetAxis("Vertical");
+	}
+	
+	void FixedUpdate () 
+	{
+
+
+		if (animator) 
+		{
+			//transform the input to worldspace. 
+			StickToWorldspace(this.transform, gameCamera.transform, ref speed);
+			
+			//update the speed of the animation
+			animator.SetFloat ("speed", speed);
+		}
+
+	}
+
+
+	public void StickToWorldspace(Transform root, Transform camera, ref float speedOut)
+	{
+		Vector3 rootDirection = root.forward;
+		
+		Vector3 stickDirection = new Vector3(horizontal, 0, vertical);
+		
+		speedOut = stickDirection.sqrMagnitude;		
+		
+		// Get camera rotation
+		Vector3 CameraDirection = camera.forward;
+		CameraDirection.y = 0.0f; // kill Y
+		Quaternion referentialShift = Quaternion.FromToRotation(Vector3.forward, Vector3.Normalize(CameraDirection));
+		
+		// Convert joystick input in Worldspace coordinates
+		Vector3 moveDirection = referentialShift * stickDirection;
+		Vector3 axisSign = Vector3.Cross(moveDirection, rootDirection);
+		
+		Debug.DrawRay(new Vector3(root.position.x, root.position.y + 2f, root.position.z), moveDirection, Color.green);
+		Debug.DrawRay(new Vector3(root.position.x, root.position.y + 2f, root.position.z), rootDirection, Color.magenta);
+		Debug.DrawRay(new Vector3(root.position.x, root.position.y + 2.5f, root.position.z), axisSign, Color.red);
+
+		float angleRootToMove;
+		if (speedOut < 0.05f) 
+		{
+			angleRootToMove = 0;
+		} else {
+			angleRootToMove = Vector3.Angle (rootDirection, moveDirection) * (axisSign.y >= 0 ? -1f : 1f);
+
+
+			if (Mathf.Abs (angleRootToMove) > 0.1f) {
+				Vector3 rotationAmount = Vector3.Lerp (Vector3.zero, new Vector3 (0f, angleRootToMove, 0f), Time.deltaTime * rotationSpeed);
+				Quaternion deltaRotation = Quaternion.Euler (rotationAmount);
+
+				this.transform.rotation = (this.transform.rotation * deltaRotation);
+			}
+		}
+	}
+
+	//checks if character is in locomotion (moving)
+	public bool IsInLocomotion()
+	{
+		return stateInfo.nameHash == m_LocomotionId;
+	}
+}
+
+	
---
+++ The Game/Assets/Scripts/Player and Camera/ThirdPersonCamera.cs
@@ -0,0 +1,106 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ThirdPersonCamera : MonoBehaviour {
+	
+	public float camSmoothDampTime = 0.15f;
+	public float radius = 2f;
+	public float mouseHorizontalSpeed = 0.14f;
+	public float mouseVerticalSpeed = 0.08f;
+
+
+	private Transform followTransform;
+	private Vector3 targetPosition;
+	private Vector3 lookDir;
+	private CamStates CamState;
+	private float latitudeOffset = 0f;
+	private float longitudeOffset = 0f;
+	private Vector3 velocityCamSmooth = Vector3.zero;
+	private Vector3 offset = new Vector3 (0f, 0f, 0f);
+
+
+	public enum CamStates
+	{
+		Follow,
+		FirstPerson,
+		Target,
+		Free
+	}
+
+
+	void Start () {
+		followTransform = GameObject.Find ("CameraFollowObject").transform;
+	}
+
+	void LateUpdate () {
+
+		if (Input.GetAxis ("Target") < 0.01f) {
+			CamState = CamStates.Follow;
+		} else {
+			CamState = CamStates.Target;
+		}
+
+		switch(CamState)
+		{
+			case CamStates.Follow:
+				//set look direction
+				lookDir = followTransform.position - this.transform.position; 
+				lookDir.y = 0.0f;
+				lookDir.Normalize ();
+				
+				updateMouse();
+
+				//target position, what position the camera wants to reach
+			targetPosition = followTransform.position + offset;
+
+			break;
+			case CamStates.Target:
+				lookDir = followTransform.forward;
+				
+			targetPosition = followTransform.position + offset ;
+			break;
+		}
+
+		//check if wall collision
+		CompensateForWalls (followTransform.position, ref targetPosition);
+		
+		//move smoothly towards targetposition
+		smoothPosition (this.transform.position, targetPosition);
+		
+		//make sure the camera is looking towards the player
+		transform.LookAt (followTransform);
+	}
+	
+	void smoothPosition(Vector3 fromPos, Vector3 toPos)
+	{
+		this.transform.position = Vector3.SmoothDamp(fromPos, toPos, ref velocityCamSmooth, camSmoothDampTime); 
+	}
+
+	private void CompensateForWalls(Vector3 fromObject, ref Vector3 toTarget)
+	{
+		// Compensate for walls between camera by casting a ray from the player to the camera
+		RaycastHit wallHit = new RaycastHit();		
+		//if a collision with something occurs, change the targetposition
+		if (Physics.Linecast(fromObject, toTarget, out wallHit)) 
+		{
+			toTarget = new Vector3(wallHit.point.x+(0.4f*wallHit.normal.x), toTarget.y, wallHit.point.z+(0.4f*wallHit.normal.z));
+		}
+	}
+
+
+	private void updateMouse()
+	{
+		float deltaY = Input.GetAxis ("Mouse Y");
+		float deltaX = Input.GetAxis ("Mouse X");
+
+		longitudeOffset = (longitudeOffset < 0.1f ? 0.1f : (longitudeOffset + -deltaY * mouseVerticalSpeed));
+		longitudeOffset = (longitudeOffset > 2.5f ? 2.5f : longitudeOffset);
+
+		latitudeOffset = (latitudeOffset + -deltaX * mouseHorizontalSpeed)%(Mathf.PI*2);
+
+		offset.y = radius * Mathf.Cos (longitudeOffset);
+		offset.x = radius * Mathf.Sin (longitudeOffset) * Mathf.Cos (latitudeOffset);
+		offset.z = radius * Mathf.Sin (longitudeOffset) * Mathf.Sin (latitudeOffset);
+	}
+
+}

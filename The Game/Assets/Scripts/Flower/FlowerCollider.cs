b828ba379772f3f23f5378f55e4bd12efc909a10
Olov Schurz <Orlaf@c-133-200.eduroam.liu.se>
2014-04-11 11:41:09 +0200

    flower update

---
+++ The Game/Assets/Scripts/Flower/FlowerCollider.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlowerCollider : MonoBehaviour {
+
+	public GUIText countText;
+	public static int count = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+		SetCountText (); 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.tag == "PickUp")
+		{
+			count++;
+			SetCountText();
+			this.gameObject.SetActive(false);
+		}
+	}
+	
+	void SetCountText()
+	{
+		countText.text = "Flowers: " + count.ToString (); 
+	}
+}

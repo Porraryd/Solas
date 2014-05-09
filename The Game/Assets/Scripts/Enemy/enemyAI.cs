38bb671fd3e9ddaa533bd076f08f6904e0230cc0
Einar Sandberg <einars92@gmail.com>
2014-05-09 12:18:50 +0200

    lite enemy, och en pausgrejs.

---
+++ The Game/Assets/Scripts/Enemy/enemyAI.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class enemyAI : MonoBehaviour 
+{
+
+	public float speed = 2f;
+
+	private Transform theTransform;
+	public Transform target;
+	private GameObject player;
+	private Animator animator;
+	
+	public float enemySpeed;
+
+	// Use this for initialization
+	void Awake ()
+	{
+		theTransform = transform;
+		animator = GetComponent<Animator>();
+
+	}
+	void Start () 
+	{
+
+		player = GameObject.FindGameObjectWithTag("Player");
+		target = player.transform;
+		
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void OnTriggerStay(Collider col)
+	{
+		
+		if (col.gameObject.tag == "Player")
+		{
+
+			Vector3 direction = target.position - theTransform.position;
+			direction.Normalize();
+			theTransform.LookAt(target);
+			//theTransform.position += direction * speed * Time.deltaTime;
+			animator.SetFloat ("speed", 1f);
+		}
+	}
+}
---
+++ The Game/Assets/Scripts/Menu/pauseGame.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class pauseGame : MonoBehaviour 
+{
+	bool pause;
+	public int checkPause;
+	GUIStyle largeFont;
+	// Use this for initialization
+	void Start () 
+	{
+		pause = false;
+		checkPause = 0;
+		largeFont = new GUIStyle();
+		largeFont.fontSize = 40;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		 if (Input.GetKeyDown("p"))
+		 {
+		 	if (checkPause % 2 == 0)
+		 	{
+             	Time.timeScale =0;
+            	 pause = true;
+            	 checkPause++;
+
+            }
+            else
+            {
+            	Time.timeScale=1;
+            	pause = false;
+            	checkPause++;
+
+            }
+         }
+	
+	}
+	void OnGUI()
+	{
+		if (pause)
+		{
+			Debug.Log("KUKEN");
+			 GUI.Label(new Rect(Screen.width/2-50, Screen.height/2-25, 100, 50), "Game paused", largeFont);
+		}
+	}
+}
